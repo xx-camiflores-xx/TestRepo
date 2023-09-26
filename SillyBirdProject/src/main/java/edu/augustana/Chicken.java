fb08ddcd6c38d5516b2d02740762854542010e27
Cami <camiflores21@augustana.edu>
2023-09-26 15:55:14 -0500

    Now with dancing chicken

---
+++ SillyBirdProject/src/main/java/edu/augustana/Chicken.java
@@ -0,0 +1,9 @@
+package edu.augustana;
+
+public class Chicken {
+
+    public void dance(){
+        System.out.println("Chicken dance!");
+    }
+
+}
--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -3,5 +3,9 @@ package edu.augustana;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello bird world!");
+
+        Chicken cluck = new Chicken();
+        cluck.dance();
     }
+
 }
